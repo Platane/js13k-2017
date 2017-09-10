12e4e759efcc262741883f9e1339358811814de7
platane <me@platane.me>
2017-09-10 15:17:37 +0200

    deploy create function

--- image-crusher-api/script/prepareFunctions.sh
+++ image-crusher-api/script/prepareFunctions.sh
@@ -10,3 +10,10 @@ cp -r lib/* dist/get/
 cp -r node_modules dist/get/node_modules
 cp package.json dist/get/package.json
 echo "exports.run = function(data,res){var err;require('./get').run().catch(function(x){err=x}).then(function(x){res.send(err?err.toString():x)})}" >> dist/get/index.js
+
+# build create function
+mkdir dist/create
+cp -r lib/* dist/create/
+cp -r node_modules dist/create/node_modules
+cp package.json dist/create/package.json
+echo "exports.run = function(data,res){var err;require('./create').run().catch(function(x){err=x}).then(function(x){res.send(err?err.toString():x)})}" >> dist/create/index.js
