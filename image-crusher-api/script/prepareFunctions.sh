f260661d7707993a4c33449369b17d3c9140228e
platane <me@platane.me>
2017-09-10 16:20:28 +0200

    :hammer: fix script

--- image-crusher-api/script/prepareFunctions.sh
+++ image-crusher-api/script/prepareFunctions.sh
@@ -9,7 +9,7 @@ mkdir dist/get
 cp -r lib/* dist/get/
 cp -r node_modules dist/get/node_modules
 cp package.json dist/get/package.json
-echo "exports.run = function(data,res){var err;require('./get').run().catch(function(x){err=x}).then(function(x){res.set('Access-Control-Allow-Origin', "*");res.set('Access-Control-Allow-Methods', 'GET');res.send(err?err.toString():x)})}" >> dist/get/index.js
+echo "exports.run = function(data,res){var err;require('./get').run().catch(function(x){err=x}).then(function(x){res.set('Access-Control-Allow-Origin','*');res.set('Access-Control-Allow-Methods','GET');res.send(err?err.toString():x)})}" >> dist/get/index.js
 
 
 
@@ -18,4 +18,4 @@ mkdir dist/create
 cp -r lib/* dist/create/
 cp -r node_modules dist/create/node_modules
 cp package.json dist/create/package.json
-echo "exports.run = function(data,res){var err;require('./create').run().catch(function(x){err=x}).then(function(x){res.set('Access-Control-Allow-Origin', "*");res.set('Access-Control-Allow-Methods', 'GET');res.send(err?err.toString():x)})}" >> dist/create/index.js
+echo "exports.run = function(data,res){var err;require('./create').run().catch(function(x){err=x}).then(function(x){res.set('Access-Control-Allow-Origin','*');res.set('Access-Control-Allow-Methods','GET');res.send(err?err.toString():x)})}" >> dist/create/index.js
--- image-crusher-worker/script/prepareFunctions.sh
+++ image-crusher-worker/script/prepareFunctions.sh
@@ -9,4 +9,4 @@ mkdir dist/worker
 cp -r lib/* dist/worker/
 cp -r node_modules dist/worker/node_modules
 cp package.json dist/worker/package.json
-echo "exports.run = function(data,res){var err;require('./run').run().catch(function(x){err=x}).then(function(x){res.set('Access-Control-Allow-Origin', "*");res.set('Access-Control-Allow-Methods', 'GET');res.send(err?err.toString():x)})}" >> dist/worker/index.js
+echo "exports.run = function(data,res){var err;require('./run').run().catch(function(x){err=x}).then(function(x){res.set('Access-Control-Allow-Origin','*');res.set('Access-Control-Allow-Methods','GET');res.send(err?err.toString():x)})}" >> dist/worker/index.js
