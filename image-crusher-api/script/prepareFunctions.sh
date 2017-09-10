3f1f9a619592dad8fd05681a4b9ccbeaece6b7b9
platane <me@platane.me>
2017-09-10 13:19:16 +0200

    :hammer: fix CI

--- image-crusher-api/script/prepareFunctions.sh
+++ image-crusher-api/script/prepareFunctions.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+rm -r dist || echo 1
+
 mkdir -p dist
 
 # build get function
@@ -7,4 +9,4 @@ mkdir dist/get
 cp -r lib/* dist/get/
 cp -r node_modules dist/get/node_modules
 cp package.json dist/get/package.json
-echo "exports.run = function(data,res){var err;require('./module/get').run().catch(function(x){err=x}).then(function(x){res.send(err?err.toString():x)})}" >> dist/get/index.js
+echo "exports.run = function(data,res){var err;require('./get').run().catch(function(x){err=x}).then(function(x){res.send(err?err.toString():x)})}" >> dist/get/index.js
