cd5f2be4a08b31137a61eeb2334cec3dcb00af97
platane <me@platane.me>
2017-09-12 00:02:18 +0200

    :rocket:

--- image-crusher-worker/src/genetic/mutate.js
+++ image-crusher-worker/src/genetic/mutate.js
@@ -169,7 +169,7 @@ export const initAdn = (PARAM: Param): ADN =>
 export const addGene = (PARAM: Param, ancestor: ADN): ADN | null => {
     if (ancestor.length === PARAM.N_CIRCLE) return null
 
-    const n = PARAM.GENE_BATCH
+    const n = 8
 
     return [...ancestor, ...Array.from({ length: n }, () => randomDot(PARAM))]
 }
