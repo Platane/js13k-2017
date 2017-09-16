5c5e4544c8e88c58446e296e016e709d5568c304
platane <me@platane.me>
2017-09-16 03:17:57 +0200

    :rocket: random default painting consistency

--- game/src/a.js
+++ game/src/a.js
@@ -92,24 +92,47 @@ const unpackADN = buffer => {
     return adn
 }
 
-const readPainting = path => {
-    // return fetch(path)
-    //     .then(x => x.text())
-    //     .then(x => unpackADN(new Uint8Array(x.split(',').map(x => +x))))
-    return fetch(path)
+const readPainting = path =>
+    fetch(path)
         .then(x => x.arrayBuffer())
         .then(x => unpackADN(new Uint8Array(x)))
+
+const hslToRgb = (h, s, l) => {
+    let r, g, b
+
+    const hue2rgb = (p, q, t) => {
+        if (t < 0) t += 1
+        if (t > 1) t -= 1
+        if (t < 1 / 6) return p + (q - p) * 6 * t
+        if (t < 1 / 2) return q
+        if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6
+        return p
+    }
+
+    const q = l < 0.5 ? l * (1 + s) : l + s - l * s
+    const p = 2 * l - q
+    r = hue2rgb(p, q, h + 1 / 3)
+    g = hue2rgb(p, q, h)
+    b = hue2rgb(p, q, h - 1 / 3)
+
+    return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)]
 }
 
-const paintings = Array.from({ length: 30 }, () =>
-    Array.from({ length: 30 }, () => ({
+const paintings = Array.from({ length: 30 }, () => {
+    const hues = [Math.random(), Math.random()]
+
+    return Array.from({ length: 30 }, (_, i) => ({
         x: Math.random() * 64,
         y: Math.random() * 64,
         r: Math.random() * 30,
-        color: rand(COLOR_PALETTE),
-        opacity: 1,
+        color: hslToRgb(
+            hues[+(i > 15)],
+            Math.random() * 0.5,
+            Math.random() * 0.6 + 0.2
+        ),
+        opacity: Math.random(),
     }))
-)
+})
 
 let step = 1
 
