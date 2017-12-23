3bae76f050c3aabd3cdec2065a4366fb7cfd3d51
platane <me@platane.me>
2017-12-23 16:28:51 +0100

    :hammer: [api] fix test

--- common/adn/pack/__tests__/pack.spec.js
+++ common/adn/pack/__tests__/pack.spec.js
@@ -5,17 +5,17 @@ import * as param from '../../../param'
 const samples = [
     [{ x: 14, y: 13, r: 2, color: 9, opacity: 1 }],
     [
-        { color: 129, opacity: 3, r: 16, x: 28, y: 35 },
-        { color: 44, opacity: 3, r: 16, x: 56, y: 39 },
+        { color: 129, opacity: 3, r: 6, x: 28, y: 35 },
+        { color: 44, opacity: 3, r: 15, x: 56, y: 39 },
         { color: 124, opacity: 2, r: 14, x: 37, y: 16 },
         { color: 199, opacity: 3, r: 6, x: 52, y: 11 },
     ],
     [
-        { color: 129, opacity: 3, r: 16, x: 28, y: 35 },
-        { color: 44, opacity: 3, r: 16, x: 56, y: 39 },
+        { color: 129, opacity: 3, r: 15, x: 28, y: 35 },
+        { color: 44, opacity: 3, r: 15, x: 56, y: 39 },
         { color: 124, opacity: 2, r: 14, x: 37, y: 16 },
         { color: 199, opacity: 3, r: 6, x: 52, y: 11 },
-        { color: 30, opacity: 0, r: 15, x: 53, y: 5 },
+        { color: 30, opacity: 0, r: 0, x: 53, y: 5 },
         { color: 87, opacity: 2, r: 15, x: 8, y: 2 },
         { color: 37, opacity: 3, r: 12, x: 21, y: 63 },
         { color: 99, opacity: 1, r: 15, x: 19, y: 57 },
