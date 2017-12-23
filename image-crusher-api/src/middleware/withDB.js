83ba9c9a7db0c9d33e1e140fed904e73d96c6f6a
platane <me@platane.me>
2017-12-23 11:57:02 +0100

    :hammer: [api] namespace

--- image-crusher-api/src/middleware/withDB.js
+++ image-crusher-api/src/middleware/withDB.js
@@ -3,7 +3,7 @@ import { config } from '../config'
 
 export const withDB = next => (data, ctx = {}) => {
     ctx.db = connectDataStore({
-        namespace: config.ENV,
+        namespace: config.ENV === 'production' ? void 0 : config.ENV,
         projectId: config.googleCloudPlatform.project_id,
         credentials: config.googleCloudPlatform,
     })
