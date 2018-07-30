cbda7b951c52de6cd22a0bbfc46ecae88c3da76e
platane <me@platane.me>
2018-07-30 20:45:03 +0100

    :shower: clean up

--- image-crusher-api/src/getJob/index.js
+++ image-crusher-api/src/getJob/index.js
@@ -5,13 +5,17 @@ import { getNextFork } from 'common/genetic/getNextFork'
 const countNode = (tree: AncestorTree) =>
     tree.children.reduce((sum, child) => sum + countNode(child), 1)
 
-const pickImage = (images: { ancestorTree: AncestorTree }): * =>
+// pick the next image
+// get one of the 8 images that have the least nodes
+const pickImage = (images: { ancestorTree: AncestorTree }[]): * =>
+    // sort all images by node count ascending
     images
         .map(image => ({
             ...image,
             n: countNode(image.ancestorTree),
         }))
         .sort((a, b) => (a.n > b.n ? 1 : -1))[
+        // take one of the first 8 images
         Math.min(images.length - 1, Math.floor(Math.random() * 8))
     ]
 
@@ -19,8 +23,6 @@ export const handler = async (_, { db }) => {
     const query = db.createQuery('image')
 
     const [images, __] = await db.runQuery(query)
-
-    // const images = await db.get(db.key(['image', 5083289484263424]))
 
     const image = pickImage(
         images.map(x => ({ ...parseImage(x), id: x[db.KEY].id }))
