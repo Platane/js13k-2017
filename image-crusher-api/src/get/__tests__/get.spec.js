827fb7aeed8f38391773b402e570f1f12c028027
platane <me@platane.me>
2018-09-16 20:38:56 +0100

    :rocket: [api] expose url, title, artist

--- image-crusher-api/src/get/__tests__/get.spec.js
+++ image-crusher-api/src/get/__tests__/get.spec.js
@@ -5,6 +5,9 @@ it(
     async () => {
         const images = await run()
 
+        expect(images[0]).toHaveProperty('title')
+        expect(images[0]).toHaveProperty('artist')
+        expect(images[0]).toHaveProperty('url')
         expect(images[0]).toHaveProperty('target')
         expect(images[0]).toHaveProperty('PARAM')
         expect(images[0]).toHaveProperty('ancestorTree')
--- image-crusher-api/src/util/dataStore/parse.js
+++ image-crusher-api/src/util/dataStore/parse.js
@@ -5,19 +5,30 @@ export const parseImage = image => {
     const PARAM: Param = JSON.parse(image.PARAM)
     const ancestorTree: AncestorTree = JSON.parse(image.ancestorTree)
 
-    return { target, PARAM, ancestorTree }
+    const { url, artist, title } = image
+
+    return { url, artist, title, target, PARAM, ancestorTree }
 }
 
 export const formatImage = ({
     target,
     PARAM,
     ancestorTree,
+    url,
+    title,
+    artist,
 }: {
+    title: string | null,
+    artist: string | null,
+    url: string | null,
     target: RImage,
     PARAM: Param,
     ancestorTree: AncestorTree,
 }) => {
     return {
+        url,
+        title,
+        artist,
         target: target.join(','),
         PARAM: JSON.stringify(PARAM),
         ancestorTree: JSON.stringify(ancestorTree),
